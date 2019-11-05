aeb3b463055ea7a725889bff282ebb37072149a2
iggy-pigy <ilgakokorisa@gmail.com>
2019-11-05 22:25:35 +0000

    wrote code to satisfy tests

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,41 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+
+function makeHalfPrice(price) {
+    return price - (price/2);
+}
+
+function countBooks(books) {
+    return books.length;
+}
+
+function isInStock(books) {
+    const book = {
+title: "The Stone Diaries",
+  author: "Carol Shields",
+  yearOfPublication: 1993,
+  quantity: 9
+};
+if (book.quantity>=1) {
+    return true;
+} else {
+    return false;
+}}
+
+function getTotalOrderPrice(price, quantity) {
+    var netoPrice = price * quantity;
+    var totalprice = netoPrice * 1.2;
+    return Math.round(totalprice * 10) / 10;
+}
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    makeHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice,
 };
